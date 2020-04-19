346683d27bba6a4ee67d22b520dd336151ebe1a2
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-04-19 15:49:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const AntiSpam = require('discord-anti-spam');
-const antiSpam = new AntiSpam({
-    warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
-    kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
-    banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
-    maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
-    warnMessage: '{@user}, Please stop spamming.', // Message that will be sent in chat upon warning a user.
-    kickMessage: '**{user_tag}** has been kicked for spamming.', // Message that will be sent in chat upon kicking a user.
-    banMessage: '**{user_tag}** has been banned for spamming.', // Message that will be sent in chat upon banning a user.
-    maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
-    maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
-    maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
-    exemptPermissions: [ 'ADMINISTRATOR'], // Bypass users with any of these permissions.
-    ignoreBots: true, // Ignore bot messages.
-    verbose: true, // Extended Logs from module.
-    ignoredUsers: [], // Array of User IDs that get ignored.
-    // And many more options... See the documentation.
-});
 
-async function apiPost(token, prefix) { 
+async apiPost(token, prefix) { 
 
 if(!token) return console.log("[spam-api]{type: error} ⚠️: make sure your give me bot token or invite bot token"); 
 if(!prefix) return console.log("[spam-api]{type: error} ⚠️: make sure your give me bot prefix"); 
